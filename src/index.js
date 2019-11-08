135525af50f9d71cf004ff358f95d0e75efa1854
marazmatique <fxpnkt@gmail.com>
2019-11-08 23:56:00 +0300

    fix export default set

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import readlineSync from 'readline-sync';
 
-const userName = () => {
+export const userName = () => {
   const name = readlineSync.question('What is your name? ');
   console.log(`Hello, ${name}!`);
 };
 
 export default userName;
-
