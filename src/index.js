de9e8a28312448669c31fe38239bf8caf2def496
marazmatique <fxpnkt@gmail.com>
2019-11-09 18:20:59 +0300

    move blank line to bottom

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import readlineSync from 'readline-sync';
 
 export const userName = () => {
   const name = readlineSync.question('What is your name? ');
-  console.log();
   console.log(`Hello, ${name}!`);
+  console.log();
 };
 
 export default userName;
