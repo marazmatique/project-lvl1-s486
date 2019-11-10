2ddc4ac604e511fa48c0d6c8d9bf4a46f6193a82
marazmatique <fxpnkt@gmail.com>
2019-11-10 15:37:12 +0300

    add all game function (incl. gamePlay)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,42 @@
 import readlineSync from 'readline-sync';
 
-export const userName = () => {
-  const name = readlineSync.question('What is your name? ');
-  console.log(`Hello, ${name}!`);
-  console.log();
+const gameHello = 'Welcome to the Brain Games!';
+const gameRules = 'Answer "yes" if number even otherwise answer "no".';
+
+export const userName = () => readlineSync.question('What is your name? ');
+
+export const userAnswer = (randomNumber) => {
+  console.log(`Question: ${randomNumber}`);
+  return readlineSync.question('Your answer: ');
 };
 
-export default userName;
+export const getRandomInt = (min = 1, max = 100) => Math.floor(Math.random()
+* (max - min + 1)) + min;
+
+export const gamePlay = (numberOfRound) => {
+  console.log();
+  console.log(gameHello);
+  console.log(gameRules);
+  console.log();
+  const name = userName();
+  console.log(`Hello, ${name}!`);
+  console.log();
+  for (let i = 1; i <= numberOfRound; i += 1) {
+    const digit = getRandomInt();
+    const correctAnswer = digit % 2 === 0 ? 'yes' : 'no';
+    const answer = userAnswer(digit);
+    if (correctAnswer !== answer) {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${name}!`);
+      console.log();
+      break;
+    }
+    if (i === numberOfRound) {
+      console.log();
+      console.log(`Congratulation, ${name}!`);
+      console.log();
+      break;
+    }
+    console.log('Correct!');
+  }
+};
