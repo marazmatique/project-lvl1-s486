d9e803530b6667c9f61b1db730d432896643f1e4
marazmatique <fxpnkt@gmail.com>
2019-11-12 17:43:03 +0300

    specil place for game options and rule (ready for added new game version in a future)

---
+++ src/game/calc.js
@@ -0,0 +1,20 @@
+import { getRandomInt } from '..';
+
+import { make } from '../makeConstructor';
+
+export const welcome = 'Welcome to the Brain Games!';
+
+export const rules = 'What is the result of the expression?';
+
+export const randomOperation = (num1 = getRandomInt(1, 25), num2 = getRandomInt(1, 25)) => {
+  switch (getRandomInt(1, 3)) {
+    case 1:
+      return make(`${num1} + ${num2}`, num1 + num2);
+    case 2:
+      return make(`${num1} - ${num2}`, num1 - num2);
+    case 3:
+      return make(`${num1} * ${num2}`, num1 * num2);
+    default:
+      return 'somthing wrong';
+  }
+};
---
+++ src/game/even.js
@@ -0,0 +1,9 @@
+import { getRandomInt } from '..';
+
+import { make } from '../makeConstructor';
+
+export const welcome = 'Welcome to the Brain Games!';
+
+export const rules = 'Answer "yes" if number even otherwise answer "no".';
+
+export const randomOperation = (num = getRandomInt(1, 100)) => make(`${num}`, (num % 2 === 0 ? 'yes' : 'no'));
