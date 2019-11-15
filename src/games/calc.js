479679234f9d40a89b6a626157215c5c09a49840
marazmatique <fxpnkt@gmail.com>
2019-11-15 18:47:49 +0300

    refactoring (without magic number^ but with eval())

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,25 +1,21 @@
 import getRandomInt from '../getRandomInt';
-import { make, expression, answer } from '../makeConstructor';
+import { make } from '../makeConstructor';
 import gamePlay from '..';
 
 export const welcome = 'Welcome to the Brain Games!';
 
 export const rules = 'What is the result of the expression?';
 
-export const choiseOperator = (symbol = getRandomInt(1, 3)) => {
-  switch (symbol) {
-    case 1:
-      return make('+', (a, b) => a + b);
-    case 2:
-      return make('-', (a, b) => a - b);
-    case 3:
-      return make('*', (a, b) => a * b);
-    default:
-      return 'somthing wrong';
-  }
+export const operList = '+-*';
+
+export const choiseRandomFromList = (list = operList) => {
+  const minOption = 0;
+  const maxOption = list.length - 1;
+  const toTakeSymbol = getRandomInt(minOption, maxOption);
+  return list[toTakeSymbol];
 };
 
 export const randomOperation = (num1 = getRandomInt(1, 25), num2 = getRandomInt(1, 25), oper =
-choiseOperator()) => make(`${num1} ${expression(oper)} ${num2}`, answer(oper)(num1, num2));
+choiseRandomFromList()) => make(`${num1} ${oper} ${num2}`, eval(`${num1} ${oper} ${num2}`));
 
 export default () => gamePlay(welcome, rules, randomOperation);
