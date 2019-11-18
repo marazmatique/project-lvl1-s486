c849d82343324ac52636dcad9e6ed0a69a901a2d
marazmatique <fxpnkt@gmail.com>
2019-11-18 14:45:44 +0300

    refactoring (use symbol \n, instead console.log())

--- src/index.js
+++ src/index.js
@@ -5,25 +5,20 @@ export default (welcome, rules, randomOperation) => {
   console.log();
   console.log(welcome);
   console.log(rules);
-  console.log();
-  const userName = readlineSync.question('What is your name? ');
+  const userName = readlineSync.question('\nWhat is your name? ');
   console.log(`Hello, ${userName}!`);
   for (let i = 1; i <= 3; i += 1) {
     const round = randomOperation();
-    console.log();
-    console.log(`Question: ${expression(round)}`);
+    console.log(`\nQuestion: ${expression(round)}`);
     const userAnswer = readlineSync.question('Your answer: ');
     if (`${answer(round)}` !== userAnswer) {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer(round)}'.`);
-      console.log();
-      console.log(`Let's try again, ${userName}!`);
-      console.log();
+      console.log(`\nLet's try again, ${userName}!\n`);
       break;
     }
     if (i === 3) {
-      console.log();
-      console.log(`Congratulation, ${userName}!`);
-      console.log();
+      console.log('Correct!');
+      console.log(`\nCongratulation, ${userName}!\n`);
       break;
     }
     console.log('Correct!');
