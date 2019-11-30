85c39695799b094eb5a18a2cdf1c45f31579e0db
marazmatique <fxpnkt@gmail.com>
2019-11-30 17:35:42 +0300

    *refactoring:* rename function 'element' to 'getElement'

--- src/games/progression.js
+++ src/games/progression.js
@@ -9,13 +9,13 @@ const lengthSequence = 10;
 const isSecret = (a, b) => a === b;
 
 const getRound = (start = getRandomInt(1, 20), step = getRandomInt(1, 5)) => {
-  const element = (counter) => start + step * counter;
+  const getElement = (counter) => start + step * counter;
   const randomSecretPosition = getRandomInt(1, lengthSequence) - 1;
   let sequence = '';
-  const answer = element(randomSecretPosition);
+  const answer = getElement(randomSecretPosition);
 
   for (let i = 0; i < lengthSequence; i += 1) {
-    sequence = isSecret(i, randomSecretPosition) ? `${sequence} ..` : `${sequence} ${element(i)}`;
+    sequence = isSecret(i, randomSecretPosition) ? `${sequence} ..` : `${sequence} ${getElement(i)}`;
   }
 
   return make(sequence, answer);
