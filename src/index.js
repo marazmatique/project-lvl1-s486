1505c90e4e650746e96e29ca62e0f1b27801cfc3
marazmatique <fxpnkt@gmail.com>
2019-12-01 17:28:47 +0300

    *refactoring:* with while + switch (tru)

--- src/index.js
+++ src/index.js
@@ -2,20 +2,29 @@ import readlineSync from 'readline-sync';
 import { car as expression, cdr as answer } from '@hexlet/pairs';
 
 export default (description, getRound) => {
-  const roundNumber = 3;
+  let roundNumber = 3;
   console.log('\nWelcome to the Brain Games!');
   console.log(description);
   const userName = readlineSync.question('\nWhat is your name? ');
   console.log(`Hello, ${userName}!`);
-  for (let i = 1; i <= roundNumber; i += 1) {
+
+  while (roundNumber > 0) {
     const round = getRound();
     console.log(`\nQuestion: ${expression(round)}`);
     const userAnswer = readlineSync.question('Your answer: ');
-    if (`${answer(round)}` !== userAnswer) {
-      return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer(round)}'.
-      \nLet's try again, ${userName}!\n`);
+    switch (true) {
+      case `${answer(round)}` !== userAnswer:
+        console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer(round)}'.
+        \nLet's try again, ${userName}!\n`);
+        roundNumber = 0;
+        break;
+      case roundNumber === 1:
+        console.log(`Correct!\n\nCongratulation, ${userName}!\n`);
+        roundNumber = 0;
+        break;
+      default:
+        console.log('Correct!');
+        roundNumber -= 1;
     }
-    console.log('Correct!');
   }
-   console.log(`\nCongratulation, ${userName}!\n`);
 };
