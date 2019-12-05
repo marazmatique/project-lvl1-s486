b9bb59cb2dbd574233a4cc64461e997fcd0eaba3
marazmatique <fxpnkt@gmail.com>
2019-12-05 18:21:31 +0300

    *refactoring:* rename function 'getRound' to 'getRoundData' (because contain two unit of data); rename 'roundNumber' to 'nRound' (according Hungarian notation); Using statement 'for' instead of 'while'

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
 import readlineSync from 'readline-sync';
 import { car as expression, cdr as answer } from '@hexlet/pairs';
 
-export default (description, getRound) => {
-  let roundNumber = 3;
+const nRound = 3;
+
+export default (description, getRoundData) => {
   console.log('\nWelcome to the Brain Games!');
   console.log(description);
   const userName = readlineSync.question('\nWhat is your name? ');
   console.log(`Hello, ${userName}!`);
 
-  while (roundNumber > 0) {
-    const round = getRound();
+  for (let i = nRound; i > 0; i -= 1) {
+    const round = getRoundData();
     console.log(`\nQuestion: ${expression(round)}`);
     const userAnswer = readlineSync.question('Your answer: ');
     switch (true) {
       case `${answer(round)}` !== userAnswer:
         console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer(round)}'.
         \nLet's try again, ${userName}!\n`);
-        roundNumber = 0;
+        i = 0;
         break;
-      case roundNumber === 1:
+      case i === 1:
         console.log(`Correct!\n\nCongratulation, ${userName}!\n`);
-        roundNumber = 0;
+        i = 0;
         break;
       default:
         console.log('Correct!');
-        roundNumber -= 1;
     }
   }
 };
