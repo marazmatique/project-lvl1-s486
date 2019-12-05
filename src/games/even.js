b9d16905620abdc907ec966c66009c7be5755a52
marazmatique <fxpnkt@gmail.com>
2019-12-05 15:46:26 +0300

    *refactoring:* rename function 'getRound' to 'getRoundData' (because contain two unit of data); taked random number for question with right call getRandomInt (without cast to string) for answer formation used check function isEven with question as argument

--- src/games/even.js
+++ src/games/even.js
@@ -1,15 +1,16 @@
 import { cons as make } from '@hexlet/pairs';
 import getRandomInt from '../getRandomInt';
 import playGame from '..';
-import isEven from '../modulo';
 
 const description = 'Answer "yes" if number even otherwise answer "no".';
 
-const getRound = (num = getRandomInt(1, 100)) => {
-  const question = `${num}`;
-  const answer = isEven(num, 2) ? 'yes' : 'no';
+const isEven = (num) => num % 2 === 0;
+
+const getRoundData = () => {
+  const question = getRandomInt(1, 100);
+  const answer = isEven(question) ? 'yes' : 'no';
 
   return make(question, answer);
 };
 
-export default () => playGame(description, getRound);
+export default () => playGame(description, getRoundData);
