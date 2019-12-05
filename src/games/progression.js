1307685dc7032dd615d5def78e3419e3c736eefc
marazmatique <fxpnkt@gmail.com>
2019-12-05 16:42:46 +0300

    *refactoring:* rename function 'getRound' to 'getRoundData' (because contain two unit of data); remove function 'getElement' (now using a simple formula for find current element); remove function 'isSecret' (now using a simple check of two numbers for equality);

--- src/games/progression.js
+++ src/games/progression.js
@@ -6,19 +6,18 @@ const description = 'What number is missing in the progression?';
 
 const lengthSequence = 10;
 
-const isSecret = (a, b) => a === b;
-
-const getRound = (start = getRandomInt(1, 20), step = getRandomInt(1, 5)) => {
-  const getElement = (counter) => start + step * counter;
+const getRoundData = () => {
+  const start = getRandomInt(1, 20);
+  const step = getRandomInt(1, 5);
   const randomSecretPosition = getRandomInt(1, lengthSequence) - 1;
   let sequence = '';
-  const answer = getElement(randomSecretPosition);
+  const answer = start + step * randomSecretPosition;
 
   for (let i = 0; i < lengthSequence; i += 1) {
-    sequence = isSecret(i, randomSecretPosition) ? `${sequence} ..` : `${sequence} ${getElement(i)}`;
+    sequence = i === randomSecretPosition ? `${sequence} ..` : `${sequence} ${start + step * i}`;
   }
 
   return make(sequence, answer);
 };
 
-export default () => playGame(description, getRound);
+export default () => playGame(description, getRoundData);
