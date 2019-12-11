2b97abb60de49ddd32d3bb92e2d57e77310816b2
marazmatique <fxpnkt@gmail.com>
2019-12-11 12:52:27 +0300

    refactoring remove function 'isEven' (to define "answer" is used "isDivided" directly )

--- src/games/even.js
+++ src/games/even.js
@@ -5,11 +5,9 @@ import isDivided from '../modulo';
 
 const description = 'Answer "yes" if number even otherwise answer "no".';
 
-const isEven = (num) => isDivided(num, 2);
-
 const getRoundData = () => {
   const question = getRandomInt(1, 100);
-  const answer = isEven(question) ? 'yes' : 'no';
+  const answer = isDivided(question, 2) ? 'yes' : 'no';
 
   return make(question, answer);
 };
