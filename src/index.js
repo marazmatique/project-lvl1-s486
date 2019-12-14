df104b0ea208e2636e1b545d19162ac4e879ffd9
marazmatique <fxpnkt@gmail.com>
2019-12-14 18:02:44 +0300

    refactoring without mutable state

--- src/index.js
+++ src/index.js
@@ -8,21 +8,21 @@ export default (description, getRoundData) => {
   console.log(description);
   const userName = readlineSync.question('\nWhat is your name? ');
   console.log(`Hello, ${userName}!`);
-  let gameEndMessage = `\nCongratulation, ${userName}!\n`;
 
   for (let i = roundsCount; i > 0; i -= 1) {
     const round = getRoundData();
     const expression = car(round);
     const answer = cdr(round);
-    const userAnswer = readlineSync.question(`\nQuestion: ${expression}\nYour answer: `);
 
-    if (answer === userAnswer) {
-      console.log('Correct!');
-    } else {
-      gameEndMessage = `'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.
-      \nLet's try again, ${userName}!\n`;
+    console.log(`\nQuestion: ${expression}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (answer !== userAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.
+      \nLet's try again, ${userName}!\n`);
       break;
     }
+
+    console.log(i > 1 ? 'Correct!' : `Correct!\n\nCongratulation, ${userName}!\n`);
   }
-  console.log(gameEndMessage);
 };
