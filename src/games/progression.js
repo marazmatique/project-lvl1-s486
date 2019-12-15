ab31182a20f133bf5d05e43dd1c4559503453c19
marazmatique <fxpnkt@gmail.com>
2019-12-15 11:28:39 +0300

    refactoring Used pure function; Take the first number for the sequence during variable declaration.

--- src/games/progression.js
+++ src/games/progression.js
@@ -6,17 +6,20 @@ const description = 'What number is missing in the progression?';
 
 const lengthSequence = 10;
 
+const getElement = (start, step, index) => start + step * index;
+
 const getRoundData = () => {
   const start = getRandomInt(1, 20);
   const step = getRandomInt(1, 5);
   const randomSecretPosition = getRandomInt(0, lengthSequence - 1);
 
-  let sequence;
-  const answer = `${start + step * randomSecretPosition}`;
+  let sequence = `${getElement(start, step, 0)}`; // take the first number for the sequence
+  const answer = `${getElement(start, step, randomSecretPosition)}`;
 
-  for (let i = 0; i < lengthSequence; i += 1) {
-    const nextElement = i === randomSecretPosition ? '..' : start + step * i;
-    sequence = i === 0 ? `${nextElement}` : `${sequence} ${nextElement}`;
+  for (let i = 1; i < lengthSequence; i += 1) {
+    const nextElement = i === randomSecretPosition ? '..' : getElement(start, step, i);
+    sequence = `${sequence} ${nextElement}`;
+    // take the rest of the numbers for the sequence (so the cycle starts at index 1, not 0)
   }
 
   return make(sequence, answer);
